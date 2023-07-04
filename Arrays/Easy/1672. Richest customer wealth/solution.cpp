f71cadbcc3fe467102f36b9cd395a95d28c29a88
Atharva Hinge <atharva.3936@gmail.com>
2023-07-04 13:47:44 +0530

    1671 Richest wealth

---
+++ Arrays/Easy/1672. Richest customer wealth/solution.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int maximumWealth(vector<vector<int>> &accounts)
+    {
+        int maxVal = INT_MIN;
+        int row = accounts.size();
+        int col = accounts[0].size();
+        sort(accounts.begin(), accounts.end());
+        for (int i = 0; i < row; i++)
+        {
+        }
+    }
+};
